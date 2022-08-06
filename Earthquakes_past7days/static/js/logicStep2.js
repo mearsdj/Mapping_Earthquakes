8f5a331d5e5354be9ad04715e60a288f050131b2
Derek <derek.mears@outlook.com>
2022-08-06 14:35:43 -0400

    updated earthquake mapping files

---
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -0,0 +1,97 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+//// Create the map object with center at the San Francisco airport.
+//let map = L.map('mapid').setView([30,30], 2);
+
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    // tileSize: 512,
+    // zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'mapbox/satellite-streets-v11',
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  "Streets": streets,
+  "Satellite": satelliteStreets
+};
+
+// create map object with a center a zoomn level, this works better for multi map setups
+let map = L.map("mapid", {
+  center: [39.5,-98.5],
+  zoom: 2,
+layers: [streets]
+});
+
+//pass our map laters into our laters control and add the layes control to the map
+L.control.layers(baseMaps).addTo(map)
+
+
+// // // Create a style for the lines.
+// let myStyle = {
+//   color: "blue",
+//   fillColor:"yellow",
+//   weight: 1
+// }
+
+//grabing geojson data from url
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson').then(function(data) {
+  // console.log(data);
+  //create a geojson layer with retreived data
+
+
+
+  // This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
+function styleInfo(feature) {
+
+  // This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+  if (magnitude === 0) {
+    return 1;
+  }
+  return magnitude * 4;
+}
+
+
+  return {
+    opacity: 1,
+    fillOpacity: 1,
+    fillColor: "#ffae42",
+    color: "#000000",
+    radius: getRadius(feature.properties.mag),
+    stroke: true,
+    weight: 0.5
+  };
+}
+  
+  
+  L.geoJSON(data
+  // We turn each feature into a circleMarker on the map.
+  ,{
+  pointToLayer: function(feature, latlng) {
+    console.log(data);
+    return L.circleMarker(latlng);
+  },
+  style:styleInfo
+} 
+  ).addTo(map);
+});
+
+
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
